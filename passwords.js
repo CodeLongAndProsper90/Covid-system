a0a185ced01bf53ad88148fe4f97b312faf35add
TheEpicProgrammer <parkalex380@gmail.com>
2020-05-20 22:54:01 +0000

    Change some stuff

---
+++ passwords.js
@@ -0,0 +1,5 @@
+function get_up() {
+  var username = document.getElementById("signup_username").value;
+  var password document.getElementById("signup_password").value;
+  console.log(username, password);
+}
